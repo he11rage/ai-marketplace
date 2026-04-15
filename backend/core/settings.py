6903f101f0002985f5a344986a40df3d3e0c46b3
Nikita Grishunin <nikitagrishn@gmail.com>
2026-04-15 15:14:20 +0500

    Add .gitignore and configure CORS and Vite API communication

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # Vite
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'core.urls'
