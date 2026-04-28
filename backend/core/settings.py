1e6d14a7fdaad7ffad0b0aef649adb5105d06be6
Nikita Grishunin <nikitagrishn@gmail.com>
2026-04-28 14:02:32 +0500

    setup docker with django, react, postgres and pgvector

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-yweelu_2lwg+i@tj$_d6+va5y6$g0kk*6&$4o=jiulv25l@tt&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,8 +42,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'corsheaders',
     'rest_framework',
+    'pgvector',
+    
+    'api.users',
+    'api.stores',
+    'api.products',
+    'api.categories',
+    'api.cart',
+    'api.orders',
+    'api.ai_chat',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +68,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", # Vite
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
@@ -82,11 +97,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
---
+++ backend/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Waiting for database to be ready..."
+sleep 5
+
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
+echo "Creating superuser if not exists..."
+python manage.py createsuperuser_if_not_exists
+
+echo "Starting development server..."
+exec python manage.py runserver 0.0.0.0:8000
