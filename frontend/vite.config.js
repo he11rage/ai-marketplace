752567a28812795f88b4968a81c9c73046953be1
Nikita Grishunin <nikitagrishn@gmail.com>
2026-04-28 14:09:14 +0500

    setup docker conf in frontend and docker compose file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
-    },
+    host: true,
+    port: 5173,
   },
 })
